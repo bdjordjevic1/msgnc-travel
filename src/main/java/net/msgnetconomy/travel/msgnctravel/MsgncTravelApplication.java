446b4658215a6aaf806eae16eb3fe98ccba20713
Boban <b.djordjevic@msgnetconomy.net>
2019-04-17 11:10:41 +0200

    TR-4 Project basic setup

---
+++ src/main/java/net/msgnetconomy/travel/msgnctravel/MsgncTravelApplication.java
@@ -0,0 +1,13 @@
+package net.msgnetconomy.travel.msgnctravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgncTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgncTravelApplication.class, args);
+	}
+
+}
